be52a04c9c8926f6620c173814d319761985268c
Jonathan <jonigl@gmail.com>
2025-03-03 20:09:34 +0100

    [main] formatting

--- 20250226/ollama-python/chat.py
+++ 20250226/ollama-python/chat.py
@@ -1,10 +1,10 @@
 from ollama import chat
 
 messages = [
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
 ]
 
 response = chat('llama3.2:1b', messages=messages)
--- 20250305/ollama-js/multimodel.ts
+++ 20250305/ollama-js/multimodel.ts
@@ -1,16 +1,16 @@
-import ollama from 'ollama'
+import ollama from "ollama";
 
 async function main() {
-  const imagePath = './img/image.png'
+  const imagePath = "./img/image.png";
   const response = await ollama.generate({
-    model: 'llava:13b',
-    prompt: 'describe this image:',
+    model: "llava:13b",
+    prompt: "describe this image:",
     images: [imagePath],
     stream: true,
-  })
+  });
   for await (const part of response) {
-    process.stdout.write(part.response)
+    process.stdout.write(part.response);
   }
 }
 
-main().catch(console.error)
+main().catch(console.error);
